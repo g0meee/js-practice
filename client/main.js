a636131b41dafe56336184f235ea56253d467ad8
g0meee <kg0meee@gmail.com>
2024-01-23 18:00:20 +0900

    JavaScript 수업 시작

---
+++ client/main.js
@@ -0,0 +1,16 @@
+const a = 10;
+
+console.log();
+
+console.log("안녕 자바스크립트");
+
+alert("마우스 우클릭을 사용할 수 없습니다.");
+//내장함수: 빌트인펑션 : 브라우저가 제공해주는 함수
+
+confirm("정말 지울꺼야..?");
+prompt("너 이름이 뭐니?");
+
+// window.confirm('정말 지울꺼야..?') 가능
+//JAvaScript 세상에서 browser == window
+
+//cdn: content delivery nerwork
